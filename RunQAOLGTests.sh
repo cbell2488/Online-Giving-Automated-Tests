d91453058e9d6eb486951fa23f8b7978647d5f96
cbell2488 <cbell@osv.com>
2016-12-12 15:48:59 -0500

    Shell file updates - updating file path

--- RunQAOLGTests.sh
+++ RunQAOLGTests.sh
@@ -12,15 +12,17 @@ echo Starting Automated Online Giving tests
 echo
 echo Running Chrome
 
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/sysadmin_login.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/org_login.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/login_donor.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/CreateOffertoryFund.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/EditOffertoryFund.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/quickGive.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/quickGiveBankAccount.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/GiveOneTimeGift.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/CreateUserAccount.feature browser=chrome url=$OLGURL
-bundle exec cucumber /Volumes/Shared/Corporate/IT/QA/Projects/Automated_Testing/Online_Giving/OLG_Capybara_Selenium_Tests/features/CreateUserAccountWithGift.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/sysadmin_login.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/org_login.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/login_donor.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/CreateOffertoryFund.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/EditOffertoryFund.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/quickGive.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/quickGiveBankAccount.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/GiveOneTimeGift.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/GiveRecurringGift.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/GivePledgeGift.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/CreateUserAccount.feature browser=chrome url=$OLGURL
+bundle exec cucumber /Users/cbell/RubymineProjects/Online-Giving-Automated-Tests/features/CreateUserAccountWithGift.feature browser=chrome url=$OLGURL
 
 #TODO: echo TestComplete
